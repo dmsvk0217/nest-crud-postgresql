417f3e6ba5d63b2fc8445a2681cdd32fa39c5def
dmsvk0217 <dmsvk0217@gmail.com>
2024-07-11 15:42:29 +0900

    FEAT: boards Read, Create 기능(Controller, Service) 구현 without DB

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllboard(): Board[] {
+    return this.boardsService.getAllboards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
 }
