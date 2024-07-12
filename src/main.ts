e557bae1eed6c43c632a93bb77463d93b335d9af
dmsvk0217 <dmsvk0217@gmail.com>
2024-07-12 18:14:43 +0900

    Logger 간단한 로그 작성

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = 3000;
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  Logger.log(`Application running on port ${port}`);
+  await app.listen(port);
 }
 
 bootstrap();
