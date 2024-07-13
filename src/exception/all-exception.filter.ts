f48cd5c176b38fc24f949f7f9ba53ff701230219
dmsvk0217 <dmsvk0217@gmail.com>
2024-07-13 18:21:34 +0900

    all-exception filter 추가

---
+++ src/exception/all-exception.filter.ts
@@ -0,0 +1,28 @@
+import {
+  Catch,
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(Error)
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
 import { Logger } from '@nestjs/common';
+import { AllExceptionsFilter } from './exception/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalFilters(new HttpExceptionFilter());
   Logger.log(`Application running on port ${port}`);
   await app.listen(port);
